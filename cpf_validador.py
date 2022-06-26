92b58a395d49ebd1000e2a93e03b6e06992bb12a
ingridvulpix <ingridalmeidafs@gmail.com>
2022-06-26 00:48:27 -0300

    integrate classes

--- cpf_validador.py
+++ cpf_validador.py
@@ -2,54 +2,56 @@ from tkinter import *
 import tkinter as tk
 import sys
 from operator import mul
-class CPF:
+
+class Legal_number:
     def __init__(self,cpf:str, dv:int):
         self.cpf = cpf
         self.dv = dv
-        self.cpf2 = list(range(10,1,-1))
         self.getDigits()
     
     def getDigits (self):
         return list(map(int,filter(lambda x:x.isdecimal(),self.cpf )))
-
 
     def dotProd (self, v1:list[int], v2:list[int]):
         return sum(map(mul, v1, v2))
 
     def areValidDigits (self):
         cpf1 = self.getDigits()
-        dv2 = self.dotProd(cpf1,self.cpf2)
+        dv2 = self.dotProd(cpf1,self.cpf_aux)
         result = dv2 % 11
         dv2 = 0 if result <= 1 else 11 - result
-        self.cpf2 = list(range(11,1,-1))
+        self.cpf_aux = list(range(11,1,-1))
         cpf1.append(dv2)
-        dv1 = self.dotProd(cpf1,self.cpf2)
+        dv1 = self.dotProd(cpf1,self.cpf_aux)
         result = dv1 % 11
         dv1 = 0 if result <= 1 else 11 - result
-        print ((dv2*10 + dv1 == self.dv))
-class Interface:
+        return (dv2*10 + dv1 == self.dv)
+
+class Interface(Legal_number):
     def __init__(self, root):
         self.root = root
         self.message =''''''
+        self.cpf_aux = list(range(10,1,-1))
         self.create_canvas()
     
     def create_canvas(self):
         self.root.title('APX3_PIG')
-        self.root.geometry('300x150')
+        self.root.geometry('500x150')
         self.root.config(bg = 'gray')
         self.create_text_box()
     
     def extract_data(self):
-        self.data_cpf = (self.text_cpf.get('1.0', 'end')).strip()
-        self.data_dv = (self.text_dv.get('1.0', 'end')).strip()
-        data = [self.data_cpf,self.data_dv]
+        self.cpf = (self.text_cpf.get('1.0', 'end')).strip()
+        self.dv = int((self.text_dv.get('1.0', 'end')).strip())
+        print(self.cpf)
+        self.validation_output()
     
     def create_text_box(self):
         
         self.text_cpf = Text(
             self.root,
             height=1,
-            width=9,
+            width=13,
             wrap='word'
         )
         self.text_dv = Text(
@@ -58,28 +60,30 @@ class Interface:
             width=2,
             wrap='word'
         )
-        self.text_cpf.pack(side=tk.LEFT, expand=TRUE)
-        self.text_dv.pack(side=tk.LEFT, expand=TRUE)
+        self.text_cpf.pack(padx=15, pady=10,side='left')
+        self.text_dv.pack(padx=5, pady=10,side='left')
         self.text_cpf.insert('end', self.message)
         self.text_dv.insert('end', self.message)
         self.create_button()
 
     def create_button (self):
-        Button(
+        self.button = Button(
         self.root,
         text='Validate CPF',
         command= self.extract_data
-    ).pack(expand=TRUE)
-        self.inside()
+    ).pack(padx=5, pady=15,side='left')
 
-    def generate_valid_cpf (self):
-        #call the other class to get valid cpf
-        self.message = '''757.331.783-29'''
-
-    def inside (self):
-        self.root.mainloop()
+    def validation_output(self): #FIXME reset output
+        valid = self.areValidDigits()
+        text = 'Valid CPF'if valid == True else 'Invalid CPF'
+        bg_color = '#bef9e2' if valid == True else '#ff5555'
+        self.output_text = Text(self.root, height=1,width=11, bg=bg_color)
+        self.output_text.insert('end',text)
+        self.output_text["state"] = DISABLED
+        self.output_text.pack(padx=10, pady=15, side='left')
 
 if __name__== '__main__':
 
     ws = Tk()
     user_cpf = Interface(ws)
+    ws.mainloop()
