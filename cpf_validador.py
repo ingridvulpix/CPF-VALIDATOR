28eeb99c7cd375b6b936abebcd12480f502842d9
ingridvulpix <ingridalmeidafs@gmail.com>
2022-06-25 21:50:11 -0300

    class CPF

--- cpf_validador.py
+++ cpf_validador.py
@@ -1,7 +1,32 @@
 from tkinter import *
 import tkinter as tk
+import sys
+from operator import mul
+class CPF:
+    def __init__(self,cpf:str, dv:int):
+        self.cpf = cpf
+        self.dv = dv
+        self.cpf2 = list(range(10,1,-1))
+        self.getDigits()
+    
+    def getDigits (self):
+        return list(map(int,filter(lambda x:x.isdecimal(),self.cpf )))
 
 
+    def dotProd (self, v1:list[int], v2:list[int]):
+        return sum(map(mul, v1, v2))
+
+    def areValidDigits (self):
+        cpf1 = self.getDigits()
+        dv2 = self.dotProd(cpf1,self.cpf2)
+        result = dv2 % 11
+        dv2 = 0 if result <= 1 else 11 - result
+        self.cpf2 = list(range(11,1,-1))
+        cpf1.append(dv2)
+        dv1 = self.dotProd(cpf1,self.cpf2)
+        result = dv1 % 11
+        dv1 = 0 if result <= 1 else 11 - result
+        print ((dv2*10 + dv1 == self.dv))
 class Interface:
     def __init__(self, root):
         self.root = root
