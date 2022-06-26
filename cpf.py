2ead63d6d55f7999fd8567bd6fc402faff2663bb
ingridvulpix <ingridalmeidafs@gmail.com>
2022-06-26 12:40:10 -0300

    generate cpf even with leading 0

--- cpf.py
+++ cpf.py
@@ -1,11 +1,14 @@
 import sys
 from operator import mul
-
+import random
+from random import randint
 
 class Cpf:
     def __init__(self,cpf:str, dv:int):
         self.cpf = cpf
         self.dv = dv
+        self.cpf_aux = list(range(10,1,-1))
+        self.cpf_aux_2 = list(range(11,1,-1))
         self.getDigits()
     
     def getDigits (self):
@@ -13,15 +16,23 @@ class Cpf:
 
     def dotProd (self, v1:list[int], v2:list[int]):
         return sum(map(mul, v1, v2))
+    
+    def generate_cpf(self):
+        self.cpf = list(map(lambda x:str(random.randint(0,9)),range(9)))
+        self.areValidDigits(True)
+        output_dv = str(self.dv)
+        output_dv = '0'+output_dv[:] if len(output_dv) == 1 else output_dv #add leading 0 if dv2=0
+        valid_cpf = ("".join(map(str,self.cpf)))+'-'+(output_dv)
+        return valid_cpf
 
-    def areValidDigits (self):
+    def areValidDigits (self,generate:bool):
         cpf1 = self.getDigits()
         dv2 = self.dotProd(cpf1,self.cpf_aux)
         result = dv2 % 11
         dv2 = 0 if result <= 1 else 11 - result
-        self.cpf_aux_2 = list(range(11,1,-1))
         cpf1.append(dv2)
         dv1 = self.dotProd(cpf1,self.cpf_aux_2)
         result = dv1 % 11
         dv1 = 0 if result <= 1 else 11 - result
+        self.dv = dv2*10 + dv1 if generate == True else self.dv
         return (dv2*10 + dv1 == self.dv)
--- main.py
+++ main.py
@@ -1,67 +1,7 @@
 from tkinter import *
-import tkinter as tk
 from cpf import Cpf
-
-class Interface(Cpf):
-    def __init__(self, root):
-        self.root = root
-        self.message =''''''
-        self.var = StringVar()
-        self.cpf_aux = list(range(10,1,-1))
-        self.create_canvas()
-    
-    def create_canvas(self):
-        self.root.title('APX3_PIG')
-        self.root.geometry('500x150')
-        self.root.config(bg = 'gray')
-        self.frame = Frame(self.root)
-        self.frame.pack()
-        self.create_label()
-        self.create_text_box()
-    
-    def extract_data(self):
-        self.cpf = (self.text_cpf.get('1.0', 'end')).strip()
-        self.dv = int((self.text_dv.get('1.0', 'end')).strip())
-        self.validation_output()
-        self.reset_btn()
-        
-    def create_label(self):
-        self.label = Label( self.frame, textvariable=self.var, relief=RAISED, font=("Ubuntu",12))
-        self.var.set("Please, insert the first 9 digits of your CPF in the first box,\n \
-            and then insert the last 2 digits in the second box")
-        self.label.pack()
-    
-    def create_text_box(self):
-        self.text_cpf = Text(self.frame,height=1,width=13,wrap='word')
-        self.text_dv = Text(self.frame,height=1,width=2,wrap='word')
-        self.text_cpf.pack(padx=15, pady=10,side='left')
-        self.text_dv.pack(padx=5, pady=10,side='left')
-        self.text_cpf.insert('end', self.message)
-        self.text_dv.insert('end', self.message)
-        self.create_button()
-
-    def create_button (self):
-            self.button = Button(self.frame,text='Validate CPF',command= self.extract_data)
-            self.button.pack(padx=5, pady=15,side='left')
-
-    def reset_btn(self):
-        self.button.config(state = DISABLED)
-        self.refresh_button = Button(self.frame, text='Refresh', command= self.clear).pack(padx=5, pady=15,side='left')
-
-    def validation_output(self):
-        valid = self.areValidDigits()
-        text = 'Valid CPF'if valid == True else 'Invalid CPF'
-        bg_color = '#bef9e2' if valid == True else '#ff5555'
-        self.output_text = Text(self.frame, height=1,width=11, bg=bg_color)
-        self.output_text.insert('end',text)
-        self.output_text["state"] = DISABLED
-        self.output_text.pack(padx=10, pady=15, side='left')
-
-
-    def clear(self):
-        self.frame.destroy()
-        self.create_canvas()
-
+from cnpj import Cnpj
+from interface import Interface
 
 if __name__== '__main__':
 
