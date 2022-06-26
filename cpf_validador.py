e8584734ad1569e0bcbd1363539ef53c9fb718bb
ingridvulpix <ingridalmeidafs@gmail.com>
2022-06-26 03:25:33 -0300

    correction size function

--- cpf_validador.py
+++ cpf_validador.py
@@ -33,6 +33,7 @@ class Interface(Legal_number):
     def __init__(self, root):
         self.root = root
         self.message =''''''
+        self.var = StringVar()
         self.cpf_aux = list(range(10,1,-1))
         self.create_canvas()
     
@@ -42,6 +43,7 @@ class Interface(Legal_number):
         self.root.config(bg = 'gray')
         self.frame = Frame(self.root)
         self.frame.pack()
+        self.create_label()
         self.create_text_box()
     
     def extract_data(self):
@@ -50,26 +52,15 @@ class Interface(Legal_number):
         self.validation_output()
         self.reset_btn()
         
+    def create_label(self):
+        self.label = Label( self.frame, textvariable=self.var, relief=RAISED, font=("Ubuntu",12))
+        self.var.set("Please, insert the first 9 digits of your CPF in the first box,\n \
+            and then insert the last 2 digits in the second box")
+        self.label.pack()
     
     def create_text_box(self):
-        var = StringVar()
-        self.label = Label( self.frame, textvariable=var, relief=RAISED, font=("Ubuntu",12))
-        var.set("Please, insert the first 9 digits of your CPF in the first box,\n \
-        and then insert last 2 digits in the second box")
-        self.label.pack()
-
-        self.text_cpf = Text(
-            self.frame,
-            height=1,
-            width=13,
-            wrap='word'
-        )
-        self.text_dv = Text(
-            self.frame,
-            height=1,
-            width=2,
-            wrap='word'
-        )
+        self.text_cpf = Text(self.frame,height=1,width=13,wrap='word')
+        self.text_dv = Text(self.frame,height=1,width=2,wrap='word')
         self.text_cpf.pack(padx=15, pady=10,side='left')
         self.text_dv.pack(padx=5, pady=10,side='left')
         self.text_cpf.insert('end', self.message)
@@ -77,18 +68,9 @@ class Interface(Legal_number):
         self.create_button()
 
     def create_button (self):
-            self.button = Button(
-            self.frame,
-            text='Validate CPF',
-            command= self.extract_data
-        )
+            self.button = Button(self.frame,text='Validate CPF',command= self.extract_data)
             self.button.pack(padx=5, pady=15,side='left')
 
-            # self.generate_button = Button(
-            # self.frame,
-            # text='Generate CPF',
-            # command= self.extract_data
-            # )
     def reset_btn(self):
         self.button.config(state = DISABLED)
         self.refresh_button = Button(self.frame, text='Refresh', command= self.clear).pack(padx=5, pady=15,side='left')
